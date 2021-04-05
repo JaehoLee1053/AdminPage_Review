67e719bea5e8bb458d5556361a92bcc90400717f
JaehoLee1053 <dlwogh1053@gmail.com>
2021-04-06 01:25:01 +0900

    Type: 10.JPA를 통한 CRUD 배우기-2
    Body
    Apply read() and update() method in test.
    
    Resolves : None
    
    See also : None

--- src/test/java/com/example/study_review/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study_review/repository/UserRepositoryTest.java
@@ -5,8 +5,10 @@ import com.example.study_review.StudyReviewApplicationTests;
 import com.example.study_review.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyReviewApplicationTests {
 
@@ -28,11 +30,26 @@ public class UserRepositoryTest extends StudyReviewApplicationTests {
         System.out.println("newUser : " + newUser);
     }
 
+    @Test
     public void read() {
-
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(selectUser -> {
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
     }
 
+    @Test
     public void update() {
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
 
     }
 
