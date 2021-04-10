334f672ad650ad50b706fa1d14441f6773e66d27
JaehoLee1053 <dlwogh1053@gmail.com>
2021-04-10 19:36:47 +0900

    Type: 9-03. Controller 추상화 적용하기
    Body
    Apply abstract in EntityController use CrudController.
    
    Resolves : None
    
    See also : None

--- src/main/java/com/example/study_review/controller/api/ItemApiController.java
+++ src/main/java/com/example/study_review/controller/api/ItemApiController.java
@@ -1,5 +1,6 @@
 package com.example.study_review.controller.api;
 
+import com.example.study_review.controller.CrudController;
 import com.example.study_review.ifs.CrudInterface;
 import com.example.study_review.model.network.Header;
 import com.example.study_review.model.network.request.ItemApiRequest;
@@ -8,38 +9,18 @@ import com.example.study_review.service.ItemApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}")
-    public Header read(@PathVariable Long id) {
-
-        return itemApiLogicService.read((id));
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-
-        return itemApiLogicService.delete(id);
+    @PostConstruct
+    public void init() {
+        this.baseService = itemApiLogicService;
     }
 
 }
